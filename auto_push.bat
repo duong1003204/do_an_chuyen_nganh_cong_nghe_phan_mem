478b08c818656cbf25e6fb8f1ec35a5f95fc5efe
duong1003204 <duong1003204@gmail.com>
2025-10-13 23:40:12 +0700

    Auto update BY Phạm Đại Dương Mon 10/13/2025 23:40:12.47

--- auto_push.bat
+++ auto_push.bat
@@ -2,7 +2,7 @@
 echo ==== AUTO GIT PUSH START ====
 cd /d "C:\Users\duong\OneDrive\Desktop\DACNCNPM"
 git add .
-git commit -m "Auto update %date% %time%"
+git commit -m "Auto update BY Phạm Đại Dương %date% %time%"
 git push origin main
 echo ==== DONE ====
 pause
--- baitaplon/trangchu/views.py
+++ baitaplon/trangchu/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from sanpham.models import DanhMuc
+from sanpham.models import DanhMuc, SanPham
 from django.db.models import Count
 
 def index(request):
     menudanhmuc = DanhMuc.objects.filter(trang_thai=True)
     danhmuc_slsp = DanhMuc.objects.filter(trang_thai=True).annotate(soluongsp=Count('sanpham'))
-    return render(request, 'index.html', {'menudanhmuc': menudanhmuc, 'danhmuc_slsp': danhmuc_slsp})
+
+    sanpham_index = SanPham.objects.filter(trang_thai=True)
+    sanpham_noibat = sanpham_index.order_by('-gia')[:8]
+    return render(request, 'index.html', {'menudanhmuc': menudanhmuc, 'danhmuc_slsp': danhmuc_slsp, 'sanpham_index': sanpham_index, 'sanpham_noibat': sanpham_noibat})
