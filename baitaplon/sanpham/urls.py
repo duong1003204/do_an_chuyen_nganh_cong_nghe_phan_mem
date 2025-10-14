ea2b0c4c4c0eb359eac8082ff16db48866f488db
duong1003204 <duong1003204@gmail.com>
2025-10-14 12:14:35 +0700

    Auto update BY Phạm Đại Dương Tue 10/14/2025 12:14:35.50

---
+++ baitaplon/sanpham/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import include, path
+
+from nguoidung import views
+from .views import chitietsanpham, colors_for_size,image_for_variant, shop
+
+urlpatterns = [
+    path('chitietsanpham/<int:id>/', chitietsanpham, name='chitietsanpham'),
+    path('ajax/colors/', colors_for_size, name='colors_for_size'),
+    path('ajax/image/', image_for_variant, name='image_for_variant'),
+    path('shop/', shop, name='shop'),
+]
--- baitaplon/sanpham/views.py
+++ baitaplon/sanpham/views.py
@@ -1,4 +1,35 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from .models import DanhMuc
+from .models import DanhMuc, SanPham, BienTheSanPham
+
+def chitietsanpham(request, id):
+    ctsp = SanPham.objects.get(id=id)
+    sizes = list(ctsp.bienthesanpham_set.values_list('kich_thuoc', flat=True).distinct())
+    colors = list(ctsp.bienthesanpham_set.values_list('mau_sac', flat=True).distinct())
+    menudanhmuc = DanhMuc.objects.filter(trang_thai=True)
+
+    return render(request, 'detail.html', {'ctsp': ctsp, 'sizes': sizes, 'colors': colors, 'menudanhmuc': menudanhmuc})
+
+def colors_for_size(request):
+    size = request.GET.get('size')
+    sp_id = request.GET.get('sp_id')
+    bien_the = BienTheSanPham.objects.filter(ma_san_pham_id=sp_id, kich_thuoc=size)
+    colors = list(bien_the.values_list('mau_sac', flat=True).distinct())
+    return JsonResponse({'colors': colors})
 
 
+def image_for_variant(request):
+    size = request.GET.get('size')
+    color = request.GET.get('color')
+    sp_id = request.GET.get('sp_id')
+    bt = BienTheSanPham.objects.filter(ma_san_pham_id=sp_id, kich_thuoc=size, mau_sac=color).first()
+    if bt and bt.hinh_anh:
+        img_url = bt.hinh_anh.url
+    else:
+        img_url = ''
+    return JsonResponse({'img_url': img_url})
+
+def shop(request):
+    menudanhmuc = DanhMuc.objects.filter(trang_thai=True)
+    sanpham = SanPham.objects.all()
+    return render(request, 'shop.html', {'menudanhmuc': menudanhmuc, 'sanpham': sanpham})
