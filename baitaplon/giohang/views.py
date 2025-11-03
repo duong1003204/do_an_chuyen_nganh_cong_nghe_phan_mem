78c2fa1df601d0cac42e602d946f334e50b07ca5
duong1003204 <duong1003204@gmail.com>
2025-11-03 13:22:38 +0700

    Auto update BY Phạm Đại Dương Mon 11/03/2025 13:22:38.06

--- baitaplon/giohang/views.py
+++ baitaplon/giohang/views.py
@@ -7,4 +7,6 @@ from django.http import HttpRequest
 # Create your views here.
 def them_vao_gio_hang():
     pass
-    
+def xem_gio_hang(request: HttpRequest):
+    san_pham = SanPham.objects.all()
+    return render(request, 'giohang.html', {'san_pham': san_pham})
