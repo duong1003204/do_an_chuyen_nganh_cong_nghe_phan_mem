877879e93c3028db13a8f960809405d4593e73f3
Hoangduc <hduc8306@gmail.com>
2025-11-03 13:55:31 +0700

    duc
    code giao dien dang ki

--- baitaplon/nguoidung/urls.py
+++ baitaplon/nguoidung/urls.py
@@ -2,8 +2,9 @@
 from django.urls import include, path
 
 from nguoidung import views
-from .views import login
+from .views import login,dangki
 
 urlpatterns = [
    path('login/', login, name='login'),
+   path('dangki/', dangki, name='dangki' ),
 ]
